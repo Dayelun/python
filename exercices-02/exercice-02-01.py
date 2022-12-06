ca41c98f10570f59b8d1b551859927ff868cc8fa
Dayelun <dylan.besnier62210@laposte.net>
2022-12-06 11:23:01 +0100

    Commit exo 2
    Réalisation de l'exercice 02-01.py.

--- exercices-02/exercice-02-01.py
+++ exercices-02/exercice-02-01.py
@@ -3,9 +3,24 @@
 # - le nombre 42 à une variable nommée `answer`
 # - le nombre d'or 1,61 à une variable nommée `golden_ratio`
 # - la chaîne de caractères "Salut " suivie de votre nom et prénom à une variable nommée `fullname`
-# - la valeur booléenne vraie si vous aimez Python ou fasse si ce n'est pas le cas, à une variable nommée `love_python`
+# - la valeur booléenne vraie si vous aimez Python ou fausse si ce n'est pas le cas, à une variable nommée `love_python`
 # - la valeur nulle `None` à une variable nommée `license_agreement`
 # Affichez ces variables
 
 # réponse 2.1
+answer = 42
+print(answer)
 
+golden_ratio = 1.61
+print(golden_ratio)
+
+fullname = "Salut Dylan Besnier"
+print(fullname)
+
+vous_aimez_Python = True
+love_python = False
+print(vous_aimez_Python)
+print(love_python)
+
+license_agreement = None
+print(license_agreement)
