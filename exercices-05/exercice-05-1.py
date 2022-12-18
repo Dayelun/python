7a7de7e332b13aaf476131b2f1ca0548c14d721e
Dayelun <dylan.besnier62210@laposte.net>
2022-12-18 10:46:19 +0100

    Commit améliorations
    Ajout des exercices 05.

---
+++ exercices-05/exercice-05-1.py
@@ -0,0 +1,8 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+def multiplication(a: float, b: float) -> float:
+    return a * b
+help(multiplication)
